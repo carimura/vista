481f9e652c5f67b4b8fcd7f514644814dd88301e
Chad Arimura <carimura@gmail.com>
2017-06-29 20:28:05 -0700

    deploy step

--- services/detect-plates/main.go
+++ services/detect-plates/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
-)
+	"net/http"
+	"os"
 
-import "github.com/openalpr/openalpr/src/bindings/go/openalpr"
+	"github.com/openalpr/openalpr/src/bindings/go/openalpr"
+)
 
 func main() {
 	alpr := openalpr.NewAlpr("us", "", "runtime_data")
@@ -20,6 +23,9 @@ func main() {
 	fmt.Println(alpr.IsLoaded())
 	fmt.Println(openalpr.GetVersion())
 
+	url := "http://wallpaper.pickywallpapers.com/1920x1080/red-alfa-romeo-4c-us-spec-in-the-city-back-view.jpg"
+	downloadFile("sample.jpg", url)
+
 	resultFromFilePath, err := alpr.RecognizeByFilePath("sample.jpg")
 	if err != nil {
 		fmt.Println(err)
@@ -33,4 +39,29 @@ func main() {
 	}
 	resultFromBlob, err := alpr.RecognizeByBlob(imageBytes)
 	fmt.Printf("%+v\n", resultFromBlob)
+}
+
+//stackoverflow.com/questions/33845770/how-do-i-download-a-file-with-a-http-request-in-go-language
+func downloadFile(filepath string, url string) (err error) {
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Writer the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
