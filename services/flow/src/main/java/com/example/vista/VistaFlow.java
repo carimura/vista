b745ca40a0e4ed686c1577ceb672c763877d1592
Owen Cliffe <owen.cliffe@oracle.com>
2017-09-21 19:16:11 +0100

    less boilerplate

--- services/flow/src/main/java/com/example/vista/VistaFlow.java
+++ services/flow/src/main/java/com/example/vista/VistaFlow.java
@@ -1,36 +1,22 @@
 package com.example.vista;
 
-import com.example.vista.messages.DetectPlateReq;
-import com.example.vista.messages.DrawReq;
-import com.example.vista.messages.ScrapeReq;
-import com.example.vista.messages.ScrapeResp;
+import com.example.vista.messages.*;
 import com.fnproject.fn.api.RuntimeContext;
 import com.fnproject.fn.api.flow.FlowFuture;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static com.example.vista.Functions.*;
+import static com.example.vista.Functions.postMessageToSlack;
 import static com.fnproject.fn.api.flow.Flows.currentFlow;
 
 /**
  * FnFlow Vista function
  */
 public class VistaFlow {
-    static {
-        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "debug");
-    }
-
-    private static final Logger log = LoggerFactory.getLogger(VistaFlow.class);
-    private static String slackChannel = "demostream";
-
-    private static void configure(RuntimeContext ctx) {
-        ctx.getConfigurationByKey("SLACK_CHANNEL").ifPresent((c) -> {
-            slackChannel = c;
-        });
-    }
 
     public void handleRequest(ScrapeReq input) throws Exception {
 
@@ -39,7 +25,7 @@ public class VistaFlow {
 
 
         // Run the scraper function to get images from Flickr
-        runScraper(input)
+        currentFlow().invokeFunction("./scraper", input, ScrapeResp.class)
                 .thenCompose(resp -> {
                     // take the list of images  and start a detect job for each image in paralle
                     List<ScrapeResp.ScrapeResult> results = resp.result;
@@ -54,33 +40,32 @@ public class VistaFlow {
                                 String id = scrapeResult.id;
 
                                 // detect plates on each image
-                                return detectPlates(new DetectPlateReq(scrapeResult.image_url, "us")).thenCompose((plateResp) -> {
-                                    if (!plateResp.got_plate) {
-                                        log.info("No plates found in {}", scrapeResult.image_url);
-                                        // bug
-                                        return currentFlow().completedValue(null);
-                                    }
+                                return currentFlow().invokeFunction("./detect-plates", new DetectPlateReq(scrapeResult.image_url, "us"), DetectPlateResp.class)
+                                        .thenCompose((plateResp) -> {
+                                            if (!plateResp.got_plate) {
+                                                log.info("No plates found in {}", scrapeResult.image_url);
+                                                // bug
+                                                return currentFlow().completedValue(null);
+                                            }
+                                            // if a plate was found trigger a job to draw the detected rectangles on the original image
+                                            // Draw rectangles renders the image to an object store returns the URL of the image
+                                            log.info("Got plate {} in {}", plateResp.plate, scrapeResult.image_url);
+                                            return currentFlow()
+                                                    .invokeFunction("./draw", new DrawReq(id, scrapeResult.image_url, plateResp.rectangles, "300x300"), DrawResp.class)
+                                                    .thenCompose((drawResp) -> {
+                                                                // Finally when the image is rendered  post an alert to twitter and slack in parallel
+                                                                log.info("Got draw response {} ", drawResp.image_url);
+                                                                return currentFlow().allOf(
+                                                                        currentFlow().invokeFunction("./alert", new AlertReq(drawResp.image_url, plateResp.plate))
+                                                                                .postImageToSlack(slackChannel,
+                                                                                        drawResp.image_url,
+                                                                                        "plate",
+                                                                                        "Found plate: " + plateResp.plate,
+                                                                                        "Have you seen this car?"));
+                                                            }
+                                                    );
 
-                                    // if a plate was found trigger a job to draw the detected rectangles on the original image
-                                    // Draw rectangles renders the image to an object store returns the URL of the image
-                                    log.info("Got plate {} in {}", plateResp.plate, scrapeResult.image_url);
-                                    return Functions
-                                            .drawRectangles(new DrawReq(id, scrapeResult.image_url, plateResp.rectangles, "300x300"))
-                                            .thenCompose((drawResp) -> {
-
-                                                        // Finally when the image is rendered  post an alert to twitter and slack in parallel
-                                                        log.info("Got draw response {} ", drawResp.image_url);
-                                                        return currentFlow().allOf(
-                                                                postAlertToTwitter(drawResp.image_url, plateResp.plate),
-                                                                postImageToSlack(slackChannel,
-                                                                        drawResp.image_url,
-                                                                        "plate",
-                                                                        "Found plate: " + plateResp.plate,
-                                                                        "Have you seen this car?"));
-                                                    }
-                                            );
-
-                                });
+                                        });
 
                                 // Collect all of the tasks into a list
                             }).collect(Collectors.toList());
@@ -103,4 +88,16 @@ public class VistaFlow {
 
     }
 
+    static {
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "debug");
+    }
+
+    private static final Logger log = LoggerFactory.getLogger(VistaFlow.class);
+    private static String slackChannel = "demostream";
+
+    private static void configure(RuntimeContext ctx) {
+        ctx.getConfigurationByKey("SLACK_CHANNEL").ifPresent((c) -> {
+            slackChannel = c;
+        });
+    }
 }
