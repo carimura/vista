d69da9360fca26c837bb62fbd942d4e47848f18d
Chad Arimura <carimura@gmail.com>
2017-09-20 20:16:15 -0700

    need to start merging these

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -1,3 +1,5 @@
+APP=${APP:-myapp}
+
 fn apps config set ${APP} PUBNUB_PUBLISH_KEY $PUBNUB_PUBLISH_KEY
 fn apps config set ${APP} PUBNUB_SUBSCRIBE_KEY $PUBNUB_SUBSCRIBE_KEY
 fn apps config set ${APP} FUNC_SERVER_URL ${FUNC_SERVER_URL}/r/${APP}
@@ -9,46 +11,40 @@ fn apps config set ${APP} STORAGE_SECRET_KEY $STORAGE_SECRET_KEY
 fn apps config set ${APP} STORAGE_BUCKET oracle-vista-out
 fn apps config set ${APP} FN_TOKEN $FN_TOKEN
 
-
-pushd ../services/alert
+cd ../services/alert
 fn routes config set ${APP} /alert TWITTER_CONF_KEY $TWITTER_CONF_KEY
 fn routes config set ${APP} /alert TWITTER_CONF_SECRET $TWITTER_CONF_SECRET
 fn routes config set ${APP} /alert TWITTER_TOKEN_KEY $TWITTER_TOKEN_KEY
 fn routes config set ${APP} /alert TWITTER_TOKEN_SECRET $TWITTER_TOKEN_SECRET
-popd
 
-pushd ../services/scraper
+cd ../scraper
 fn routes config set ${APP} /scraper FLICKR_API_KEY $FLICKR_API_KEY
 fn routes config set ${APP} /scraper FLICKR_API_SECRET $FLICKR_API_SECRET
-popd
 
-pushd ../services/post-slack
+cd ../post-slack
 fn routes config set ${APP} /post-slack SLACK_API_TOKEN $SLACK_API_TOKEN
-popd
 
 sync_async_fns="alert detect-faces detect-plates draw"
 
 # the flow version requires some functions to be sync
 # the normal version requires them to be async
 #
-if [ ${VISTA_MODE} = flow ] ; then
+if [[ ${VISTA_MODE} == "flow" ]]
+then
    echo configuring apps for flow
    # just the flow  version
    fn apps config set ${APP} NO_CHAIN true
 
    for func in $sync_async_fns ; do
-      pushd ../services/$func
-        fn routes update  ${APP} $func --type sync
-      popd
+     cd ../$func
+     fn routes update  ${APP} $func --type sync
    done
 else
    echo configuring apps for async
    fn apps config set ${APP} NO_CHAIN ""
 
-     for func in $sync_async_fns ; do
-         pushd ../services/$func
-           fn routes update  ${APP} ${func} --type async
-         popd
-      done
-
+  for func in $sync_async_fns ; do
+    cd ../$func
+    fn routes update  ${APP} ${func} --type async
+  done
 fi
--- services/draw/func.rb
+++ services/draw/func.rb
@@ -65,8 +65,8 @@ payload["rectangles"].each do |coords|
     draw_string = "rectangle #{coords["startx"]}, #{coords["starty"]}, #{coords["endx"]}, #{coords["endy"]}"
     c.fill('none')
     is_nude = payload["is_nude"] || "false"
-    c.stroke('red')
-    c.strokewidth(5)
+    c.stroke('yellow')
+    c.strokewidth(10)
     c.draw draw_string
   end 
 end
