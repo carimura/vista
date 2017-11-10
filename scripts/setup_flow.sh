7a9a165010a44db7c1360d6a11bde089073c272e
Owen Cliffe <owen.cliffe@oracle.com>
2017-11-10 16:19:07 +0000

    updating images and FDK versions

--- scripts/setup_flow.sh
+++ scripts/setup_flow.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo running completer
-docker rm -f completer || true 
+echo running flow-service
+docker rm -f floww || true 
 docker run --rm  -d -p 8081:8081 \
        -e DB_URL=inmem: \
        -e API_URL="http://$DOCKER_LOCALHOST:8080/r" \
-       --name completer \
-       fnproject/completer:latest
+       --name flow \
+       fnproject/flow:latest
 
 
-echo running completer ui
-docker rm -f completerui || true 
-docker run --name completerui --rm   -p3000:3000 -e API_URL=http://$DOCKER_LOCALHOST:8080 -d -e COMPLETER_BASE_URL=http://$DOCKER_LOCALHOST:8081 fnproject/completer:ui
+echo running flow ui
+docker rm -f flowui || true 
+docker run --name flowui --rm   -p3000:3000 -e API_URL=http://$DOCKER_LOCALHOST:8080 -d -e COMPLETER_BASE_URL=http://$DOCKER_LOCALHOST:8081 fnproject/flow:ui
 
-echo open http://localhost:3000 for the completer UI
+echo open http://localhost:3000 for the flow UI
