4e7f18a24c0085f057dea8e8947b083b0ddbf423
Chad Arimura <carimura@gmail.com>
2018-01-23 18:22:53 -0800

    small fixes

--- services/scraper/func.rb
+++ services/scraper/func.rb
@@ -31,15 +31,9 @@ end
 STDERR.puts "Found #{photos.size} images, posting to #{ENV["FUNC_SERVER_URL"]}/#{service_to_call}"
 threads = []
 
-blacklist_photos = ['35331390846']
-
 payloads = []
 photos.each do |photo|
-  if blacklist_photos.include?(photo.id)
-    image_url = "https://farm3.staticflickr.com/2175/5714544755_e5dc8e6ede_b.jpg"
-  else
-    image_url = FlickRaw.url_c(photo)
-  end
+  image_url = FlickRaw.url_c(photo)
 
   payload = {:id => photo.id, 
              :image_url => image_url,
@@ -49,13 +43,15 @@ photos.each do |photo|
   STDERR.puts "got image #{payload[:id]} : #{payload[:image_url]}"
   payloads.push(payload)
 
-  if ENV["NO_CHAIN"]
-    result={}
-    result[:result] = payloads
-    puts result.to_json 
-  else
+  if !ENV["NO_CHAIN"]
     RestClient.post(ENV["FUNC_SERVER_URL"] + "/" + service_to_call, payload.to_json, headers={content_type: :json, accept: :json})
   end
+end
+
+if ENV["NO_CHAIN"]
+  result={}
+  result[:result] = payloads
+  puts result.to_json
 end
 
 STDERR.puts "done"
