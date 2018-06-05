dd2b7dff4aaceb55724b1c65a4ba2921792ec42b
Owen Cliffe <owen.cliffe@oracle.com>
2018-06-05 12:12:29 +0100

    updating setenv for new CLI changes

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -1,28 +1,28 @@
 APP=${APP:-vista}
 
-fn apps config set ${APP} PUBNUB_PUBLISH_KEY $PUBNUB_PUBLISH_KEY
-fn apps config set ${APP} PUBNUB_SUBSCRIBE_KEY $PUBNUB_SUBSCRIBE_KEY
-fn apps config set ${APP} FUNC_SERVER_URL ${FUNC_SERVER_URL}/r/${APP}
-fn apps config set ${APP} MINIO_SERVER_URL $MINIO_SERVER_URL
-fn apps config set ${APP} COMPLETER_BASE_URL $COMPLETER_BASE_URL
-fn apps config set ${APP} STORAGE_ACCESS_KEY $STORAGE_ACCESS_KEY
-fn apps config set ${APP} STORAGE_SECRET_KEY $STORAGE_SECRET_KEY
-fn apps config set ${APP} STORAGE_BUCKET oracle-vista-out
-fn apps config set ${APP} FN_TOKEN $FN_TOKEN
+fn config app ${APP} PUBNUB_PUBLISH_KEY $PUBNUB_PUBLISH_KEY
+fn config app ${APP} PUBNUB_SUBSCRIBE_KEY $PUBNUB_SUBSCRIBE_KEY
+fn config app ${APP} FUNC_SERVER_URL ${FUNC_SERVER_URL}/r/${APP}
+fn config app ${APP} MINIO_SERVER_URL $MINIO_SERVER_URL
+fn config app ${APP} COMPLETER_BASE_URL $COMPLETER_BASE_URL
+fn config app ${APP} STORAGE_ACCESS_KEY $STORAGE_ACCESS_KEY
+fn config app ${APP} STORAGE_SECRET_KEY $STORAGE_SECRET_KEY
+fn config app ${APP} STORAGE_BUCKET oracle-vista-out
+fn config app ${APP} FN_TOKEN $FN_TOKEN
 
 
 cd ../services/alert
-fn routes config set ${APP} /alert TWITTER_CONF_KEY $TWITTER_CONF_KEY
-fn routes config set ${APP} /alert TWITTER_CONF_SECRET $TWITTER_CONF_SECRET
-fn routes config set ${APP} /alert TWITTER_TOKEN_KEY $TWITTER_TOKEN_KEY
-fn routes config set ${APP} /alert TWITTER_TOKEN_SECRET $TWITTER_TOKEN_SECRET
+fn config route ${APP} /alert TWITTER_CONF_KEY $TWITTER_CONF_KEY
+fn config route ${APP} /alert TWITTER_CONF_SECRET $TWITTER_CONF_SECRET
+fn config route ${APP} /alert TWITTER_TOKEN_KEY $TWITTER_TOKEN_KEY
+fn config route ${APP} /alert TWITTER_TOKEN_SECRET $TWITTER_TOKEN_SECRET
 
 cd ../scraper
-fn routes config set ${APP} /scraper FLICKR_API_KEY $FLICKR_API_KEY
-fn routes config set ${APP} /scraper FLICKR_API_SECRET $FLICKR_API_SECRET
+fn config route ${APP} /scraper FLICKR_API_KEY $FLICKR_API_KEY
+fn config route ${APP} /scraper FLICKR_API_SECRET $FLICKR_API_SECRET
 
 cd ../post-slack
-fn routes config set ${APP} /post-slack SLACK_API_TOKEN $SLACK_API_TOKEN
+fn config route ${APP} /post-slack SLACK_API_TOKEN $SLACK_API_TOKEN
 
 sync_async_fns="alert detect-plates draw"
 
@@ -37,14 +37,14 @@ then
 
    for func in $sync_async_fns ; do
      cd ../$func
-     fn routes update  ${APP} $func --type sync
+     fn update route ${APP} $func --type sync
    done
 else
    echo "------- Configuring App for Async --------"
-   fn apps config set ${APP} NO_CHAIN ""
+   fn config app ${APP} NO_CHAIN ""
 
   for func in $sync_async_fns ; do
     cd ../$func
-    fn routes update  ${APP} ${func} --type async
+    fn update route ${APP} ${func} --type async
   done
 fi
