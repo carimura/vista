a103b81679399a544f40e55e773adc4f16bd417f
Denis Makogon <lildee1991@gmail.com>
2018-10-04 20:44:33 +0300

    Ongoing fixes

--- services/publish/func.py
+++ services/publish/func.py
@@ -2,6 +2,8 @@ import fdk
 import ujson
 import os
 
+from fdk import fixtures
+
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub
 
@@ -13,7 +15,21 @@ pnconfig.ssl = False
 pn = PubNub(pnconfig)
 
 
-def main(ctx, data=None, **kwargs):
+TEST_MODE = os.environ.get("TEST_MODE", "false")
+
+
+# very basic test, will fail if env var TEST_MODE is not set to true
+# we don't really want to test pubnub client in unit tests.
+async def test_override_content_type(aiohttp_client):
+    with open("payload.json", "r") as payload_file:
+        call = await fixtures.setup_fn_call(
+            aiohttp_client, handle, json=ujson.load(payload_file))
+        content, status, headers = await call
+
+        assert 200 == status
+
+
+def handle(ctx, data=None, **kwargs):
     payload = None
     if data and len(data) > 0:
         payload = ujson.loads(data)
@@ -27,15 +43,18 @@ def main(ctx, data=None, **kwargs):
         image_key = obj.get("key")
         if all((bucket_name, image_key)):
             url = "{0}/{1}/{2}".format(
-                os.environ["MINIO_SERVER_URL"],
+                os.environ.get("MINIO_SERVER_URL"),
                 bucket_name, image_key)
 
             message = {'url': url, 'id': image_key}
             message_json = ujson.dumps(message)
 
-            (pn.publish().channel(bucket_name).
-             message([message_json]).use_post(True).sync())
+            if TEST_MODE not in ['true', '1', 't', 'y', 'yes',
+                             'yeah', 'yup', 'certainly', 'uh-huh']:
+
+                (pn.publish().channel(bucket_name).
+                 message([message_json]).use_post(True).sync())
 
 
 if __name__ == "__main__":
-    fdk.handle(main)
+    fdk.handle(handle)
