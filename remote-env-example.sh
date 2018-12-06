a3cc652b484f7c825f3391d5d72504dcfc953968
Denis Makogon <lildee1991@gmail.com>
2018-12-06 18:33:56 +0300

    Make Vista work with remote Fn
    Closes: #51

--- remote-env-example.sh
+++ remote-env-example.sh
@@ -2,29 +2,36 @@
 
 set -ex
 
-# on linux/bmc find this out with "docker inspect --type container -f '{{.NetworkSettings.Gateway}}' functions"
-export BASE_HOST=docker.for.mac.localhost
-export FN_API_ENDPOINT=http://${BASE_HOST}:8080
-export FN_INVOKE_ENDPOINT=${FN_API_ENDPOINT}/invoke
-export DOCKER_LOCALHOST=${BASE_HOST}
+# remote distributed Fn
+export FN_API_ENDPOINT=http://<your-api-service>:80
+export FN_INVOKE_ENDPOINT=http://<your-lb-servie>:80/invoke
+
+# all of this relates to Flow that you're suppose to run locally
 export COMPLETER_BASE_URL=http://${DOCKER_LOCALHOST}:8081
 
+# pubnub configuration
 export PUBNUB_PUBLISH_KEY=".."
 export PUBNUB_SUBSCRIBE_KEY=".."
 
+# twitter configuration
 export TWITTER_CONF_KEY="..."
 export TWITTER_CONF_SECRET="..."
 export TWITTER_TOKEN_KEY="..."
 export TWITTER_TOKEN_SECRET="..."
 
+# flickr configuration
 export FLICKR_API_KEY="..."
 export FLICKR_API_SECRET="..."
 
+# slack configuration
 export SLACK_API_TOKEN="...."
 
-# minio configuration
-export INSTALL_MINIO=1
+# ignore minio
+export INSTALL_MINIO=0
+export DOCKER_LOCALHOST=docker.for.mac.localhost
 export MINIO_SERVER_URL=http://${DOCKER_LOCALHOST}:9000
+
+# S3 access configuration
 export STORAGE_ACCESS_KEY="DEMOACCESSKEY"
 export STORAGE_SECRET_KEY="DEMOSECRETKEY"
 export STORAGE_BUCKET="oracle-vista-out"
